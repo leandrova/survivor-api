e21220e4651c5631d943913977ab2023345b770e
Leandro Viana <leandroviana@gmail.com>
2016-07-01 12:22:07 -0300

    Incluindo logs

--- source/app/app.js
+++ source/app/app.js
@@ -29,17 +29,22 @@ app.get('/', function(req, res) {
 });
 
 app.post('/authentication', function(req, res) {
+	console.log('=========== Auth ===========')
 	var authentication = require('./services/authentication');	
-
 	var auth = new authentication();
-
 	auth.checkChannel(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkChannel', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				auth.authentication(
 					req.body,
 					function(resss){
+						console.log('====');
+						console.log('authentication', req.body);
+						console.log('Result', resss);
 						if (resss.authentication) {
 							res.cookie('service-session', String(resss.authentication.token), { httpOnly: true, maxAge: 60 * 60 * 24 * 1 });
 						}
@@ -56,21 +61,29 @@ app.post('/authentication', function(req, res) {
 });
 
 app.get('/map', function(req, res) {
+	console.log('=========== MAP ===========')
 	res.setHeader('Content-Type', 'application/json');
-
 	var map = require('./services/map');
 	var mp = new map();
-
 	mp.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				mp.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						mp.list(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('list', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
@@ -87,21 +100,29 @@ app.get('/map', function(req, res) {
 });
 
 app.get('/rounds', function(req, res) {
+	console.log('=========== Rounds ===========')
 	res.setHeader('Content-Type', 'application/json');
-
 	var rounds = require('./services/rounds');
 	var round = new rounds();
-
 	round.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				round.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						round.list(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('list', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
@@ -117,21 +138,29 @@ app.get('/rounds', function(req, res) {
 });
 
 app.post('/rounds/detail', function(req, res) {
+	console.log('=========== Rounds Detail ===========');
 	res.setHeader('Content-Type', 'application/json');
-
 	var rounds = require('./services/rounds/detail');
 	var round = new rounds();
-
 	round.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				round.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						round.listDetail(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('listDetail', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
@@ -147,21 +176,29 @@ app.post('/rounds/detail', function(req, res) {
 });
 
 app.get('/classification', function(req, res) {
+	console.log('=========== Classification ===========');
 	res.setHeader('Content-Type', 'application/json');
-
 	var classification = require('./services/classification');
 	var clas = new classification();
-
 	clas.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				clas.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						clas.list(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('list', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
@@ -178,18 +215,23 @@ app.get('/classification', function(req, res) {
 });
 
 app.get('/players', function(req, res) {
+	console.log('=========== Players ===========');
 	res.setHeader('Content-Type', 'application/json');
-
 	var players = require('./services/players');
 	var play = new players();
-
 	play.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				play.list(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('list', req.body);
+						console.log('Result', resss);
 						res.send(resss);
 					}
 				);
@@ -204,21 +246,29 @@ app.get('/players', function(req, res) {
 });
 
 app.post('/betting', function(req, res) {
+	console.log('=========== Betting ===========');
 	res.setHeader('Content-Type', 'application/json');
-
 	var betting = require('./services/betting/list');
 	var bet = new betting();
-
 	bet.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				bet.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						bet.list(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('list', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
@@ -235,21 +285,29 @@ app.post('/betting', function(req, res) {
 });
 
 app.post('/betting/bet', function(req, res) {
+	console.log('=========== Betting Bet ===========');
 	res.setHeader('Content-Type', 'application/json');
-
 	var betting = require('./services/betting/bet');
 	var bet = new betting();
-
 	bet.checkSession(
 		req.headers,
 		function(ress){
+			console.log('====');
+			console.log('checkSession', req.headers);
+			console.log('Result', ress);
 			if (ress.lines) {
 				bet.correntRound(
 					req.body, ress.results,
 					function(resss){
+						console.log('====');
+						console.log('correntRound', req.body);
+						console.log('Result', resss);
 						bet.bet(
 							req.body, resss,
 							function(ressss){
+								console.log('====');
+								console.log('bet', req.body);
+								console.log('Result', ressss);
 								res.send(ressss);
 							}
 						);
