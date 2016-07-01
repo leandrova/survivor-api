eeab538eb8765c817ff5e4c0bde9c73e80462b04
Leandro Viana <leandroviana@gmail.com>
2016-07-01 16:22:56 -0300

    Atualização do script

--- process/node-app.sh
+++ process/node-app.sh
@@ -4,12 +4,13 @@ TIME=$(date +"%H-%M-%S")
 ARQ_EM_PROCE=$(ps -ef | grep app.js | wc -l)
 LOG_NODE="/home/leandroviana/survivor-api/process/logs/app-log-$TODAY-$TIME.log"
 LOG_PROC="/home/leandroviana/survivor-api/process/logs/app-start-$TODAY-$TIME.log"
+LOG_CRON="/home/leandroviana/survivor-api/process/logs/cron.log"
 if [ $ARQ_EM_PROCE -eq 1 ]
 then
   echo "# Executando Deploy"
-  echo "Processo iniciando em $TODAY as $TIME" > $LOG_PROC &
+  echo "Processo iniciando em $TODAY as $TIME" > $LOG_PROC
   node /home/leandroviana/survivor-api/app/app.js > LOG_NODE &
 else
-  echo "# APP em execucao"
+  echo "# APP em execucao as $TODAY $TIME" > LOG_CRON
 fi
 
