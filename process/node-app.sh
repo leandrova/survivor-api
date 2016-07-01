77b538ed370fee280457e3f90e379701dc5b72a8
Leandro Viana <leandroviana@gmail.com>
2016-07-01 15:55:41 -0300

    Ajuste na data e home do script de start do serviço.

--- process/node-app.sh
+++ process/node-app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-TODAY=$(date +"%Y%m%d")
-TIME=$(date +%s)
+TODAY=$(date +"%d-%m-%Y")
+TIME=$(date +"%H-%M-%S")
 ARQ_EM_PROCE=$(ps -ef | grep app.js | wc -l)
 if [ $ARQ_EM_PROCE -eq 1 ]
 then
   echo "# Executando Deploy"
-  node ../app/app.js > ./logs/app_$TODAY_$TIME_.log &
+  echo "Processo iniciando em ".$TODAY." as ".$TIME > ./logs/app_start_$TODAY_$TIME_.log &
+  node ../app/app.js > ./logs/app_log_$TODAY_$TIME_.log &
 else
   echo "# APP em execucao"
 fi
