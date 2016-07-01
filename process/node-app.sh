c29bc240a112cd82def5a365f154ac86ce6db011
Leandro Viana <leandroviana@gmail.com>
2016-07-01 16:15:16 -0300

    Atualização do script de execução da aplicação

--- process/node-app.sh
+++ process/node-app.sh
@@ -2,11 +2,13 @@
 TODAY=$(date +"%d-%m-%Y")
 TIME=$(date +"%H-%M-%S")
 ARQ_EM_PROCE=$(ps -ef | grep app.js | wc -l)
+LOG_NODE="./logs/app-log-$TODAY-$TIME.log"
+LOG_PROC="./logs/app-start-$TODAY-$TIME.log"
 if [ $ARQ_EM_PROCE -eq 1 ]
 then
   echo "# Executando Deploy"
-  echo "Processo iniciando em ".$TODAY." as ".$TIME > ./logs/app_start_$TODAY_$TIME_.log &
-  node ../app/app.js > ./logs/app_log_$TODAY_$TIME_.log &
+  echo "Processo iniciando em $TODAY as $TIME" > $LOG_PROC &
+  node ../app/app.js > LOG_NODE &
 else
   echo "# APP em execucao"
 fi
